694dd42ba9ee40482b524fde7704f550340b989f
DennisLudmann <dennisludmann@gmail.com>
2022-01-26 12:35:37 +0100

    search method, input, render

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
 
-async function getSynonymes(){  // async is needed, because we dont just wanna process the code the "normal" way but wait
+async function getSynonymes() {  // async is needed, because we dont just wanna process the code the "normal" way but wait
 
-    let url = 'https://www.openthesaurus.de/synonyme/search?q=test&format=application/json';
-       let response = await fetch(url); //it always takes some time to download the data - so we need to wait for it 'await'
-                                         //fetch is needed to get/fetch the data from the server.
-       let responseAsJson = await response.json(); // .json() is needed to get "workable" info as return
-    
-       console.log('The response is:', responseAsJson);  
-                                                         
-    document.getAnimations('container').innerHTML = responseAsJson;
+     let query =  document.getElementById('inputQuery').value;
+    let url = `https://www.openthesaurus.de/synonyme/search?q=${query}&format=application/json`;
+    let response = await fetch(url); //it always takes some time to download the data - so we need to wait for it 'await'
+    //fetch is needed to get/fetch the data from the server.
+    let responseAsJson = await response.json(); // .json() is needed to get "workable" info as return
+    let synsets = responseAsJson['synsets'];
+    renderSynests(synsets);
+}
+
+function renderSynests(synsets) {
+    let container = document.getElementById('container');
+    container.innerHTML =`<div>We have Loaded <b>${synsets.length} </b> Sets of Synomymes ${synsets['id']}</div>`
+
+    for (let i = 0; i < synsets.length; i++) {
+        const synset = synsets[i];
+        let terms = synset['terms'];    // is am Array (so we need a forloop within our forloop) so we can get the arrays out of our array
+        container.innerHTML += `<h2 id="h2"> Sets of Synomymes</h2>`
+        
+        for (let j = 0; j < terms.length; j++) {
+            const term = terms[j];
+            container.innerHTML += `<div>${term['term']}</div>`
+        }
+    }
+
 }
